feaf24c9f207f6758d0ad5b85252071209c722db
DharmaTeja Vytla <teja6d@gmail.com>
2024-06-28 15:15:05 +0100

    chore: Update npm dependency installation in test.yaml workflow

--- jest.config.ts
+++ jest.config.ts
@@ -11,9 +11,6 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   preset: 'ts-jest'
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  
 }
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
