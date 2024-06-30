fe9f4c80ee55371da903f3703686e7b3a4a6dcf9
DharmaTeja Vytla <teja6d@gmail.com>
2024-06-30 18:40:33 +0100

    Revert "Merge branch 'nextjs-practice' of https://github.com/teja4d/zoro-ui into nextjs-practice"
    This reverts commit 0ad3ecdf1af4576b615603d140964998ec62f0b8, reversing
    changes made to 9a745671e88ee51db2a063ec0ddc7e7b55cc4433.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,36 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import Head from 'next/head';
+
+import './globals.css';
+
 import Navbar from '../components/nav-bar/nav-bar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'ZORO UK',
+  description: 'ZORO Uk Take Home Test',
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <>
+    <html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />{" "}
-        {/* Ensure the path starts with a slash for root directory */}
+        <link rel="icon" href="./favicon.ico" />
       </Head>
-      <Navbar />
-      <div className="h-screen flex flex-col">
+      <body className="h-screen flex flex-col">
+        <Navbar />
         <div className="flex-grow flex items-center justify-center">
           {children}
         </div>
-      </div>
-    </>
+      </body>
+    </html>
   );
 }
