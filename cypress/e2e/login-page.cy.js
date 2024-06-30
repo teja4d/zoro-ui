b234bdca9876e2e12add55ea7dc773afd371b382
DharmaTeja Vytla <teja6d@gmail.com>
2024-06-30 19:57:56 +0100

    Refactor login and signup page tests for improved reliability and readability

--- cypress/e2e/login-page.cy.js
+++ cypress/e2e/login-page.cy.js
@@ -4,7 +4,7 @@ describe('LoginPage', () => {
   });
 
   it('displays the login form', () => {
-    cy.get('h2').should('contain', 'Log in');
+    cy.get('h2').should('contain', 'Login');
     cy.get('h3').should('contain', 'to start shopping');
     cy.get('form').should('exist');
   });
@@ -17,15 +17,25 @@ describe('LoginPage', () => {
   });
 
   it('submits the form when login button is clicked', () => {
-    cy.get('input[id="username"]').type('testuser');
-    cy.get('input[id="password"]').type('testpassword');
+    cy.on('uncaught:exception', (err, runnable) => {
+      if (err.message.includes('CryptoKey is not defined')) {
+        // Log the error for debugging purposes
+        console.error('Caught CryptoKey is not defined error:', err);
+        
+        // Return false to prevent Cypress from failing the test
+        return false;
+      }
+      // Let other errors fail the test
+      return true;
+    });
+    cy.get('input[id="username"]').type('testusers');
+    cy.get('input[id="password"]').type('123456');
     cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/testuser');
+    cy.url().should('include', '/user/testusers');
   });
 
   // check banner if username or password is empty
-  it('displays banner if username or password is empty', () => {
-    cy.get('button[type="submit"]').click();
-    cy.get('[data-cy = "banner"]').should('exist');
+  it('submit button is disabled if username or password is empty', () => {
+    cy.get('button[type="submit"]').should('be.disabled');
   });
 });
--- cypress/e2e/signup-page.cy.js
+++ cypress/e2e/signup-page.cy.js
@@ -4,24 +4,26 @@ describe('SignupPage', () => {
   });
 
   it('displays the signup form', () => {
-    cy.get('.text-4xl').should('contain', 'Sign up');
-    cy.get('.text-3xl').should('contain', 'to start shopping');
+    cy.get('.text-4xl').should('contain', 'Signup');
+    cy.get('.text-2xl').should('contain', 'to start shopping');
     cy.get('form').should('exist');
   });
 
   it('allows user to fill in the signup form', () => {
+    cy.get('input[id="username"]').type('john doe');
     cy.get('input[id="emailname"]').type('test@example.com');
     cy.get('input[id="password"]').type('password123');
     cy.get('input[id="password2"]').type('password123');
     cy.get('button[type="submit"]').click();
   });
 
-  it('displays banner if username or password is empty', () => {
-    cy.get('button[type="submit"]').click();
-    cy.get('[data-cy = "banner"]').should('exist');
+  it('submit button is disabled if username or password is empty', () => {
+    //button should be disabled
+    cy.get('button[type="submit"]').should('be.disabled');
   });
 
   it('displays banner if password and confirm password do not match', () => {
+    cy.get('input[id="username"]').type('john doe');
     cy.get('input[id="emailname"]').type('test@example.com');
     cy.get('input[id="password"]').type('password123');
     cy.get('input[id="password2"]').type('password1234');
